aa81d6f9dce2285a74a0d381c54e2311dc9a0c0b
AHMK <arthurhmk@gmail.com>
2020-05-18 02:20:24 -0300

    repos module

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Repository{
+  repos:{
+    name:string;
+    html_url:string;
+    description:string;
+  }
+}
+
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
@@ -8,9 +16,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ReposComponent implements OnInit {
 
+  collaborators = ['arthurhmk','fabio-alves','jhonnatthan'];
+  repositories: Array<Repository> = [];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.collaborators.forEach(str=>{
+      this.http.get<Repository>("https://api.github.com/users/"+str+"/repos")
+      .subscribe(data=>{
+        this.repositories.push(data);
+      });
+    });
   }
 
 }
